1a272cf75b167dff30fbb9af5653cc528da3319f
ZofiaOkninska <33523092+ZofiaOkninska@users.noreply.github.com>
2025-01-15 18:50:11 +0000

    Implement simble web page with Flask

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# devops-project
+# Project description
 
-This is a project with Dockerized simple web site
+This is a project with Dockerized simple web site.
+
+## Requirements
+
+Flask is required to run, so at first run: `python3 -m pip install Flask`
+
+## Functionality 
+
+The website is hosted locally at the port 5000.
+The website greets you when you provide your name in the url like:
+`localhost:5000/hello/<your name here>`
--- web-app.py
+++ web-app.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/hello/<name>')
+def hello_name(name) :
+	return 'Hello %s!' % name
+
+@app.route('/')
+# '/' URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
+
+if __name__ == '__main__':
+	# run() method of Flask class runs the application
+	# on the local development server.
+	app.run(host='0.0.0.0', port=5000, debug=True)
